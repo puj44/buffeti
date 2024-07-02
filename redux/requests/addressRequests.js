e0c0e53e615c921200498392fe67a10ca3e92f19
puj44 <pujan007mm@gmail.com>
2024-07-03 01:05:15 +0530

    typo

--- redux/requests/addressRequests.js
+++ redux/requests/addressRequests.js
@@ -2,22 +2,22 @@ import {post,get,remove,put} from "../api/apiWrapper"
 
 export const getAddressesApi = ({payload}) =>{
     
-    return get("/profile/addresses/list",payload?.location ?? null, payload?.token ?? null);
+    return get("/profile/address/list",payload?.location ?? null, payload?.token ?? null);
 }
 export const addAddressesApi = ({payload}) =>{
     let obj = JSON.parse(JSON.stringify(payload));
     delete obj.location;
     delete obj.token;
-    return post("/profile/addresses/add",obj,payload?.location ?? null, payload?.token ?? null);
+    return post("/profile/address/add",obj,payload?.location ?? null, payload?.token ?? null);
 }
 export const editAddressesApi = ({payload}) =>{
     let obj = JSON.parse(JSON.stringify(payload));
     delete obj.location;
     delete obj.token;
     delete obj.id;
-    return put("/profile/addresses/edit/"+payload?.id,obj,payload?.location ?? null, payload?.token ?? null);
+    return put("/profile/address/edit/"+payload?.id,obj,payload?.location ?? null, payload?.token ?? null);
 }
 export const deleteAddressesApi = ({payload}) =>{
     
-    return remove("/profile/addresses/delete/"+payload?.id,payload?.location ?? null, payload?.token ?? null);
+    return remove("/profile/address/delete/"+payload?.id,payload?.location ?? null, payload?.token ?? null);
 }
