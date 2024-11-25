90c4c97636856a09a363c1fcf831d890c9ca6c8e
drach <drachit631@gmail.com>
2024-11-25 13:16:43 +0530

    fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,27 +4,25 @@ import flowbite from "flowbite-react/tailwind";
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx,html}",
+    "./index.html",
     "./components/**/*.{js,ts,jsx,tsx,mdx,html}",
     "./app/**/*.{js,ts,jsx,tsx,mdx,html}",
     flowbite.content(),
   ],
   theme: {
-    screens:{
-      'xs': '368px',
-      '2sm': '486px',
-      'sm': '640px',
+    screens: {
+      xs: "368px",
+      "2sm": "486px",
+      sm: "640px",
 
-      'md': '787px',
+      md: "787px",
 
-      'lg':'1050px',
+      lg: "1050px",
 
-      'xl': '1280px',
+      xl: "1280px",
 
-      '2xl': '1536px',
-    
-    }
+      "2xl": "1536px",
+    },
   },
-  plugins: [
-    flowbite.plugin(),
-  ],
+  plugins: [flowbite.plugin()],
 };
