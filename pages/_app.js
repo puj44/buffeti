ad5517ee0ddd61fb795850280ae927f60a9cfcc1
drach <drachit631@gmail.com>
2024-11-25 13:29:11 +0530

    css fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,8 @@
-
-
 import LaunchingSoon from "@/components/LaunchingSoon";
 import Layout from "@/components/Layout";
 import "@/styles/globals.scss";
 import { Provider, useDispatch } from "react-redux";
-import {store, wrapper} from "../redux/store";
+import { store, wrapper } from "../redux/store";
 import { getData, setData } from "@/redux/reducers/homeReducer";
 import { getDataApi } from "@/redux/requests/homeRequests";
 import { hasCookie } from "cookies-next";
@@ -14,57 +12,39 @@ import { END } from "redux-saga";
 import { getCartDetails } from "@/redux/reducers/cartReducer";
 
 function App({ Component, pageProps, props }) {
-  const {locations} = props;
+  const { locations } = props;
 
   const dispatch = useDispatch();
-  useEffect(()=>{
-    if(process.env.NEXT_PUBLIC_ENVIRONMENT !== "PROD"){
-
-      dispatch(getTokenStatus());
-      dispatch(getCartDetails());
-    }
-  },[dispatch])
-  dispatch(setData({statusCode:200, data:{locations}}))
-  return(
+  useEffect(() => {
+    dispatch(getTokenStatus());
+    dispatch(getCartDetails());
+  }, [dispatch]);
+  dispatch(setData({ statusCode: 200, data: { locations } }));
+  return (
     // <Provider store={store}>
-        <div className="page-content min-h-[80vh]">
-          {
-            process.env.NEXT_PUBLIC_ENVIRONMENT === "PROD" ? 
-            <LaunchingSoon/> :
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-          }
-        
-        </div>
-  )
+    <div className="page-content min-h-[80vh]">
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
+  );
 }
 
 export async function fetchData() {
-  await store.dispatch(getData())
+  await store.dispatch(getData());
   await store.dispatch(END);
   await store.sagaTask.toPromise();
   return true;
 }
 
-
-
-App.getInitialProps = async(props)=>{
-  if(process.env.NEXT_PUBLIC_ENVIRONMENT !== "PROD"){
-    await fetchData();
-    const {locations} = await store?.getState()?.home;
-    return {
-    props:{
-      locations
-    }
-  }
-  }
+App.getInitialProps = async (props) => {
+  await fetchData();
+  const { locations } = await store?.getState()?.home;
   return {
-    props:{
-      locations:[]
-    }
-  }
-}
+    props: {
+      locations,
+    },
+  };
+};
 
 export default wrapper.withRedux(App);
-
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import Banners from "@/components/Homepage/Banners";
 // import SuggestivePackage from "@/components/Homepage/SuggestivePackage";
-import { wrapper} from "../redux/store";
+import { wrapper } from "../redux/store";
 import { useEffect, useState } from "react";
-import {  getPackagesData, setFilters } from "@/redux/reducers/packageReducer";
+import { getPackagesData, setFilters } from "@/redux/reducers/packageReducer";
 import { END } from "redux-saga";
 import { getFiltersApi } from "@/redux/requests/packageRequests";
-import { getCookie} from "cookies-next";
+import { getCookie } from "cookies-next";
 import SnackboxCard from "@/components/Homepage/SnackboxCard";
 import MiniThaliCard from "@/components/Homepage/MiniThaliCard";
 import { useSelector } from "react-redux";
@@ -13,18 +13,23 @@ import { useSelector } from "react-redux";
 import dynamic from "next/dynamic";
 import PackageCardLoader from "@/components/SkeletonLoader/PackageCardLoader";
 // const ViewCart = dynamic(()=> import("@/components/Common/ViewCart"),{ssr:false});
-const SuggestivePackage = dynamic(()=> import("@/components/Homepage/SuggestivePackage"),{
-  loading:()=>
-    <div className="flex flex-wrap sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3  justify-between w-full gap-6">
-    <PackageCardLoader /><PackageCardLoader /><PackageCardLoader />
-    </div>
-});
-
+const SuggestivePackage = dynamic(
+  () => import("@/components/Homepage/SuggestivePackage"),
+  {
+    loading: () => (
+      <div className="flex flex-wrap sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3  justify-between w-full gap-6">
+        <PackageCardLoader />
+        <PackageCardLoader />
+        <PackageCardLoader />
+      </div>
+    ),
+  }
+);
 
 export default function Home(props) {
-  const {noOfPeople,packages,filters} = props;
+  const { noOfPeople, packages, filters } = props;
   const [isCartAdded, setIsCartAdded] = useState(false);
-  const {cartDetails} = useSelector((state) => state.cart);
+  const { cartDetails } = useSelector((state) => state.cart);
   // useEffect(()=>{
   //   if(Object.keys(cartDetails ?? {}).length > 0){
   //     setIsCartAdded(true);
@@ -34,18 +39,27 @@ export default function Home(props) {
   // },[cartDetails])
   return (
     <div
-      className={`${ process.env.NEXT_PUBLIC_ENVIRONMENT === "DEV" ? "flex flex-col md:gap-6 gap-4 py-8  page-spacing ":""}`} style={{alignItems:"stretch"}}
+      className={`flex flex-col md:gap-6 gap-4 py-8  page-spacing`}
+      style={{ alignItems: "stretch" }}
     >
-          <Banners />
-          {packages?.length > 0 && <SuggestivePackage data={packages[0]} noOfPeople={noOfPeople} filters={filters} />}
-          <h4 className="font-semibold hidden page-title 2xl:text-center sm:block">Snack box And Mini Meals</h4>
-          <div className="max-w-[1120px] w-full flex self-center">
-            <div className="flex flex-col sm:flex-row w-full  gap-6 ">
-              <SnackboxCard />
-              <MiniThaliCard />
-            </div>
-          </div>
-          {/* {
+      <Banners />
+      {packages?.length > 0 && (
+        <SuggestivePackage
+          data={packages[0]}
+          noOfPeople={noOfPeople}
+          filters={filters}
+        />
+      )}
+      <h4 className="font-semibold hidden page-title 2xl:text-center sm:block">
+        Snack box And Mini Meals
+      </h4>
+      <div className="max-w-[1120px] w-full flex self-center">
+        <div className="flex flex-col sm:flex-row w-full  gap-6 ">
+          <SnackboxCard />
+          <MiniThaliCard />
+        </div>
+      </div>
+      {/* {
             isCartAdded &&
             <div className="block md:hidden">
               <ViewCart show={true} />
@@ -55,14 +69,19 @@ export default function Home(props) {
   );
 }
 
-async function fetchData(store,location) {
-  const packageFilters = (await getFiltersApi({payload:{menuOption:"click2cater", location}}))?.data; 
+async function fetchData(store, location) {
+  const packageFilters = (
+    await getFiltersApi({ payload: { menuOption: "click2cater", location } })
+  )?.data;
   const filters = packageFilters?.data?.filters;
   await store.dispatch(setFilters(packageFilters));
   if (filters && filters?.categories) {
-
     if (Object.keys(filters?.categories).length > 0) {
-      const payload = {menuOption:"click2cater?category=" + Object.keys(filters.categories)[0], location};
+      const payload = {
+        menuOption:
+          "click2cater?category=" + Object.keys(filters.categories)[0],
+        location,
+      };
       await store.dispatch(getPackagesData(payload));
       await store.dispatch(END);
       await store.sagaTask.toPromise();
@@ -70,31 +89,36 @@ async function fetchData(store,location) {
   }
   const packages = await store.getState()?.packages;
   return {
-    filters:filters ?? {},
-    packages:Object.keys(packages?.packages ?? {})?.length >0 ? Object.values(packages?.packages) : {}
+    filters: filters ?? {},
+    packages:
+      Object.keys(packages?.packages ?? {})?.length > 0
+        ? Object.values(packages?.packages)
+        : {},
   };
 }
 
-export const getServerSideProps =  wrapper.getServerSideProps(
-  (store) => async(props) =>{
-    const location = await getCookie("location",{req:props?.req,res:props?.res});
+export const getServerSideProps = wrapper.getServerSideProps(
+  (store) => async (props) => {
+    const location = await getCookie("location", {
+      req: props?.req,
+      res: props?.res,
+    });
     let noOfPeople = getCookie("no_of_people") ?? "10_20";
-    if(noOfPeople){
-        const qty = Number(noOfPeople);
-        if(qty > 20 && qty <= 30){
-            noOfPeople = "20_30";
-
-        }else if(qty  > 30){
-            noOfPeople = "30_plus"
-        }
+    if (noOfPeople) {
+      const qty = Number(noOfPeople);
+      if (qty > 20 && qty <= 30) {
+        noOfPeople = "20_30";
+      } else if (qty > 30) {
+        noOfPeople = "30_plus";
+      }
     }
-    let data = await fetchData(store,location);
+    let data = await fetchData(store, location);
     data.noOfPeople = noOfPeople;
     data.location = location ?? null;
     return {
-      props:{
-        ...data
-      }
-    }
+      props: {
+        ...data,
+      },
+    };
   }
-)
+);
