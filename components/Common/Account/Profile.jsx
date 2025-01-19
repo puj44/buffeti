441d63af2b0a5d02b10fd093a8dc868447f29666
puj44 <pujan007mm@gmail.com>
2025-01-19 18:56:14 +0530

    cart fixes and account update

---
+++ components/Common/Account/Profile.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+
+function Profile({ details }) {
+  const [isEdited, setEdited] = useState(false);
+  const [profileData, setProfileData] = useState({ ...details });
+  useEffect(() => {
+    if (JSON.stringify(profileData) !== JSON.stringify(details)) {
+      setEdited(true);
+    }
+  }, [profileData]);
+
+  return (
+    <div className="grid grid-flow-row gap-3">
+      <div className="grid grid-flow-row sm:grid-cols-2 gap-3">
+        <div className="flex flex-col gap-1 ">
+          <p className="font-semibold">{"Name"}</p>
+          <div className="input-container">
+            <input
+              value={profileData?.name ?? ""}
+              onChange={(e) => {
+                setProfileData(e.target.value);
+              }}
+            />
+          </div>
+        </div>
+        <div className="flex flex-col gap-1 ">
+          <p className="font-semibold">{"Contact Number"}</p>
+          <div className="input-container">
+            <input
+              value={profileData?.mobile_number ?? ""}
+              onChange={(e) => {
+                setProfileData(e.target.value);
+              }}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col gap-1 ">
+        <p className="font-semibold">{"Email"}</p>
+        <div className="input-container">
+          <input
+            value={profileData?.email ?? ""}
+            onChange={(e) => {
+              setProfileData(e.target.value);
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
