7ebd935e6c1dc5f82480be909ab38f2d786159e7
puj44 <pujan007mm@gmail.com>
2025-02-04 02:01:45 +0530

    verify otp done

--- redux/reducers/customerReducer.js
+++ redux/reducers/customerReducer.js
@@ -1,9 +1,13 @@
-const { createSlice } = require("@reduxjs/toolkit");
+import { put } from "../api/apiWrapper";
+
+const { createSlice, createAsyncThunk } = require("@reduxjs/toolkit");
+
 
 export const customerSlice = createSlice({
     name:"customer",
     initialState:{
-        profile:{}
+        profile:{},
+        profileResponse:{}
     },
     reducers:{
         getProfile:(state) =>{
@@ -12,9 +16,22 @@ export const customerSlice = createSlice({
             
             state.profile = action?.payload?.data?.profile ?? {};
         },
-    }
+        updateProfileAction:(state,action) =>{
+
+        },
+        updateProfileResponse:(state,{payload}) =>{
+            const message = payload?.status === 200 || payload?.statusCode === 200 ? "Profile updated successfully!" : "Something went wrong!"
+            state.profileResponse = {
+                success:payload?.status === 200 || payload?.statusCode === 200 ?true:false,
+                message:message
+            };
+        },
+        resetProfile:(state) =>{
+            state.profileResponse = {};
+        }
+    },
 });
 
-export const {getProfile,setProfile} = customerSlice.actions;
+export const {getProfile,setProfile,resetProfile,updateProfileAction,updateProfileResponse} = customerSlice.actions;
 
 export default customerSlice.reducer;
